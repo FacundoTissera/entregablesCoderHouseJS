fe7ca2da4f900d4179fa3d0e8144192ac59578de
Facundo Tissera <facundotisserasorribas@gmail.com>
2022-02-21 11:56:33 -0300

    se le agrega ciclo for y condicionales a la funcion

--- SimuladorInteractivo/index.js
+++ SimuladorInteractivo/index.js
@@ -2,17 +2,33 @@
 
 
 
-
 function notaFinal() {
-    let primerTrimestre = parseInt(prompt('nota del primer trimestre'));
+    let primerTrimestre =  parseInt(prompt('nota del primer trimestre'));
     let segundoTrimestre = parseInt(prompt('nota del segundo trimestre'));
     let tercerTrimestre = parseInt(prompt('nota del tercer trimestre'));
+    
+    
+    const nota = [primerTrimestre, segundoTrimestre, tercerTrimestre];
+    let sum = 0;
+    
+    for (let i = 0; i < nota.length; i++) {
+        sum += nota[i];
+        
+    }
 
-        let nota = primerTrimestre + segundoTrimestre + tercerTrimestre;
-
-        let resultado = nota / 3;
-
-        return alert(`Tu Nota final es de ${resultado}`) 
-}
+    let final = sum / 3
+        console.log(final);
+    
+        if (final < 5.9) {
+    
+            alert(`su nota es ${final} por lo tanto esta desaprobado`) 
+    
+        } else {
+    
+            alert(`su nota es de ${final} por lo tanto esa aprobado`) 
+    
+        }
+    
+ }
 
 notaFinal();
